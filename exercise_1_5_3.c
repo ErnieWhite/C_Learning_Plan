784746e5f924dfb1f75b52c18bcf8707af9be209
Archie White <ernie.white.68@gmail.com>
2025-12-30 05:21:36 +0000

    Completed exercise 1.5 part 3.  Experiment with escape sequences: \\n, \\t, \\.

---
+++ exercise_1_5_3.c
@@ -0,0 +1,13 @@
+/* 
+ * Exercise 1.5 part 3
+ *
+ * Experiment with escape sequences: \n \t, \\.
+ */
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello,\\tWorld!\n");
+    printf("Hello,\tWorld!\n");
+
+    return 0;
+}
