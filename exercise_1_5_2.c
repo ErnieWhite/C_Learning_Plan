1f3867540579130921a10d557ab5d713cdef2f75
Archie White <ernie.white.68@gmail.com>
2025-12-30 05:16:50 +0000

    Completed exercise 1.5 2

---
+++ exercise_1_5_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hi, my name is Ernie.\n");
+    printf("This is my first C program.\n");
+    printf("I just wanted to say\n");
+    printf("\nHello, World!\n");
+
+    return 0;
+}
